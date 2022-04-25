fba383f00554ffc462467b4b77bb45b614c5aafe
bash7325 <bash7325@gmail.com>
2022-04-25 13:15:11 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let button = document.querySelector("#button");
+
+const determineLameness = (person) => {
+    console.log(person)
+    let lameness = false;
+    if (person.toLowerCase() === "corey" || person.toLowerCase() === "corey post") {
+        lameness = true;
+    }
+    return lameness;
+}
+
+
+button.addEventListener("click", function () {
+    let inputText = document.getElementById("inputText").value;
+    let lameAnswer = determineLameness(inputText);
+    let lamenessDisplay = "";
+
+    if (lameAnswer) {
+        lamenessDisplay = "You are a lame person";
+    } else {
+        lamenessDisplay = "You are not a lame person";
+    }
+    console.log(lamenessDisplay);
+    document.getElementById("output").innerHTML = lamenessDisplay;
+});
