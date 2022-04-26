1776e500f47fa5de37bfe1aa56f4832cf2c27586
Bret Fernberg <49325125+bretyo@users.noreply.github.com>
2022-04-26 15:01:18 -0600

    uses the data

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 let button = document.querySelector("#button");
-console.log(lamenessData)
-const determineLameness=  (person)=> {
-    console.log(person)
-    console.log(lamenessData)
+
+const getPersonsLameness =(person_id)=>{
     let lameness = false;
-    if (person.toLowerCase() === "corey" || person.toLowerCase() === "corey post") {
-        lameness = true;
-    }
+    lamenessData.People.forEach(person => {
+        if(person.person_id === person_id && person.is_lame === 1){
+            console.log("TRUE")
+            lameness =  true;
+        }
+    })
+    return lameness
+}
+
+const determineLameness=  (person)=> {
+    let lameness = false;
+
+    lamenessData.Names.forEach(element => {
+        if(element.name.toLowerCase()=== person.toLowerCase()){
+            lameness = getPersonsLameness(element.person_id)
+        }
+    });
     return lameness;
 }
 
@@ -21,6 +33,5 @@ button.addEventListener("click", function() {
     } else {
         lamenessDisplay = "You are not a lame person";
     }
-    console.log(lamenessDisplay);
     document.getElementById("output").innerHTML = lamenessDisplay;
 });
