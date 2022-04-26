3dfbd4c0238ab2bdece6604996c5ec67b8c4d23f
Bret Fernberg <49325125+bretyo@users.noreply.github.com>
2022-04-26 13:53:47 -0600

    Adding Blake to Lameness Data

--- lamenessData.js
+++ lamenessData.js
@@ -4,6 +4,11 @@ const lamenessData = {
             "person_id": 1,
             "base_name": "Corey",
             "is_lame": 1
+        },
+        {
+            "person_id": 2,
+            "base_name": "Blake",
+            "is_lame": 1
         }
     ],
     "Names":[
@@ -16,6 +21,16 @@ const lamenessData = {
             "name_id": 2,
             "person_id": 1,
             "name": "Corey Post"
+        },
+        {
+            "name_id": 3,
+            "person_id": 2,
+            "name": "Blake"
+        },
+        {
+            "name_id": 4,
+            "person_id": 2,
+            "name": "Blake Stevens"
         }
     ],
     "LamenessReasons":[
@@ -23,6 +38,11 @@ const lamenessData = {
             "lameness_reason_id": 1,
             "person_id": 1,
             "lameness_reason": "Blames others."
+        },
+        {
+            "lameness_reason_id": 2,
+            "person_id": 2,
+            "lameness_reason": "Didn't play disc golf."
         }
     ]
 }
